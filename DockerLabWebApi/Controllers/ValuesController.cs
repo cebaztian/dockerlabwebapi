e39773c1594da42ac465496321a97f3f79cce17b
sebastian.burgos <sebastian.burgos@ceiba.com.co>
2017-07-03 16:30:08 -0600

    First Commit

--- DockerLabWebApi/Controllers/ValuesController.cs
+++ DockerLabWebApi/Controllers/ValuesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -12,7 +13,7 @@ namespace DockerLabWebApi.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return GetSystemDetails();
         }
 
         // GET api/values/5
@@ -34,6 +35,24 @@ namespace DockerLabWebApi.Controllers
         // DELETE api/values/5
         public void Delete(int id)
         {
+        }
+
+        private List<string> GetSystemDetails()
+        {
+            var systemInfo = new List<string>();
+            systemInfo.Add( Environment.UserName); // User name of PC
+            //systemInfo.Add(getOSInfo(); // OS version of pc
+            systemInfo.Add(Environment.MachineName);// Machine name
+            string OStype = "";
+            if (Environment.Is64BitOperatingSystem) { OStype = "64-Bit, "; } else { OStype = "32-Bit, "; }
+            OStype += Environment.ProcessorCount.ToString() + " Processor";
+            systemInfo.Add(OStype); // Processor type
+            var toalRam = new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory;
+            double toal = Convert.ToDouble(toalRam / (1024 * 1024));
+            int t = Convert.ToInt32(Math.Ceiling(toal / 1024).ToString());
+            systemInfo.Add(t.ToString() + " GB");// ram detail
+
+            return systemInfo;
         }
     }
 }
